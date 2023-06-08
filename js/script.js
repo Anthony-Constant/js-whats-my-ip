d6be75c75a82654a23f0f17c557ce4724d418a12
Anthony Constant <83375598+Anthony-Constant@users.noreply.github.com>
2023-06-08 17:42:23 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+    // Fetch the user's IP address and IP location using an API
+    fetch('https://api64.ipify.org?format=json')
+      .then(response => response.json())
+      .then(data => {
+        const ipAddress = data.ip;
+        const ipAddressElement = document.getElementById('ip-address');
+        ipAddressElement.textContent = ipAddress;
+
+        // Fetch the IP information including location and ISP
+        fetch(`https://ipapi.co/${ipAddress}/json/`)
+          .then(response => response.json())
+          .then(data => {
+            const location = `${data.city}, ${data.region}, ${data.country_name}`;
+            const locationElement = document.getElementById('location');
+            locationElement.textContent = location;
+
+            const isp = data.org;
+            const ispElement = document.getElementById('isp');
+            ispElement.textContent = isp;
+          })
+          .catch(error => console.log('Error:', error));
+      })
+      .catch(error => console.log('Error:', error));
+
+     // Copy IP Address Button Function
+     const copyIPButton = document.getElementById('copy-ip-button');
+     const ipAddressElement = document.getElementById('ip-address');
+     
+     copyIPButton.addEventListener('click', () => {
+       const ipAddress = ipAddressElement.innerText;
+     
+       navigator.clipboard.writeText(ipAddress)
+         .then(() => {
+           copyIPButton.textContent = 'Copied';
+           copyIPButton.disabled = true;
+           copyIPButton.className = 'btn btn-success';
+     
+           setTimeout(() => {
+             copyIPButton.textContent = 'Copy IP Address';
+             copyIPButton.disabled = false;
+             copyIPButton.className = 'btn btn-primary';
+           }, 2000); // Reset to original state after 2000 milliseconds (2 seconds)
+         })
+         .catch((error) => {
+           console.error('Failed to copy IP address:', error);
+         });
+     });
+     
+     
+     
+
+      
